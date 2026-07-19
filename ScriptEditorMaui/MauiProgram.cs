651b58f83e85af7aa7eb5107ad753b67ab30bc89
Yonnier08 <146913572+Yonnier08@users.noreply.github.com>
2026-07-19 01:07:45 -0400

    Create MauiProgram.cs

---
+++ ScriptEditorMaui/MauiProgram.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
+namespace ScriptEditorMaui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
