8fccbf9fc99bee936f80a408e08c3196fcf9e6c1
Yonnier08 <146913572+Yonnier08@users.noreply.github.com>
2026-07-19 22:37:31 -0400

    Update MainPage.xaml.cs

--- ScriptEditorMaui/MainPage.xaml.cs
+++ ScriptEditorMaui/MainPage.xaml.cs
@@ -5,5 +5,19 @@ public partial class MainPage : TabbedPage
 	public MainPage()
 	{
 		InitializeComponent();
+		this.Loaded += MainPage_Loaded;
+	}
+
+	private async void MainPage_Loaded(object? sender, EventArgs e)
+	{
+		this.Loaded -= MainPage_Loaded;
+
+		string logPath = Path.Combine(FileSystem.CacheDirectory, "crash_log.txt");
+		if (File.Exists(logPath))
+		{
+			string content = await File.ReadAllTextAsync(logPath);
+			await DisplayAlert("Último crash detectado", content, "OK");
+			File.Delete(logPath);
+		}
 	}
 }
